83827473d9770bd6c8d36cd39fdb9cb36c6f91dc
omarabir <omarabir@gmail.com>
2026-01-08 12:48:31 +0600

    solved problem 5

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ function reverseString(text) {
   }
 }
 // reverseString("hello")
-/---------------------------------------------------/;
+
+// /---------------------------------------------------/;
+
 // Problem 2: Count Vowels in a String
 function countVowels(text) {
   let count = 0;
@@ -17,7 +19,9 @@ function countVowels(text) {
   return count;
 }
 // countVowels("programming")
-/---------------------------------------------------/;
+
+// /---------------------------------------------------/;
+
 // Problem 3: Check for Palindrome
 function isPalindrome(str) {
   let reversed = str.split("").reverse().join("");
@@ -26,7 +30,8 @@ function isPalindrome(str) {
 
 // isPalindrome("madam");
 // isPalindrome("hello");
-/---------------------------------------------------/;
+
+// /---------------------------------------------------/;
 
 // Problem 4: Find the Maximum Number
 function findMax(arr) {
@@ -42,4 +47,22 @@ function findMax(arr) {
 
 // findMax([5, 1, 9, 3]); 
 
-/---------------------------------------------------/;
+// /---------------------------------------------------/;
+
+// Problem 5: Remove Duplicates from an Array
+function removeDuplicates(arr) {
+  let unique = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!unique.includes(arr[i])) {
+      unique.push(arr[i]);
+    }
+  }
+  return unique;
+}
+
+// removeDuplicates([1, 2, 2, 3, 4, 4]);
+
+// /---------------------------------------------------/;
+
+
